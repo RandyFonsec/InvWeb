9fdb4439985db71b9fdefda54b4e237989e7fd67
RandyFonsec <49202815+RandyFonsec@users.noreply.github.com>
2023-10-23 12:31:31 -0600

    Barra busqueda

--- pulperia-react/src/InventarioPage.js
+++ pulperia-react/src/InventarioPage.js
@@ -1,93 +1,71 @@
 import React, { useState, useEffect } from 'react';
-import './styles.css'; // Importa el archivo CSS
+import './styles.css';
 import ProductTable from './ProductTable.js';
-import EditProductModal from './EditProductModal.js'; // Importa el nuevo componente
+import EditProductModal from './EditProductModal.js';
 import AddProductModal from './AddProductModal.js';
 
 function InventarioPage() {
-
-
-  const [isModalOpen, setModalOpen] = useState(false);
+  const [isEditModalOpen, setEditModalOpen] = useState(false);
   const [selectedProduct, setSelectedProduct] = useState(null);
-  const [products, setProducts] = useState([]);
   const [isAddModalOpen, setAddModalOpen] = useState(false);
+  const [products, setProducts] = useState([]);
+  const [searchTerm, setSearchTerm] = useState(''); // Estado para el término de búsqueda
+  const [searchResults, setSearchResults] = useState([]); // Estado para los resultados de la búsqueda
 
-  // ...
   useEffect(() => {
-    fetch('http://localhost:3000/productos') // Realiza una solicitud a la ruta de la API '/productos'
+    fetch('http://localhost:3000/productos')
       .then((response) => response.json())
       .then((data) => {
         setProducts(data);
+        setSearchResults(data); // Inicialmente, muestra todos los productos como resultados
       })
       .catch((error) => {
         console.error('Error al obtener la lista de productos:', error);
       });
   }, []);
 
-  // Función para abrir el modal de edición
   const openEditModal = (product) => {
     setSelectedProduct(product);
-    setModalOpen(true);
+    setEditModalOpen(true);
   };
-   const openAddModal = () => {
+
+  const openAddModal = () => {
     setAddModalOpen(true);
   };
 
-
-  // Función para cerrar el modal
   const closeEditModal = () => {
     setSelectedProduct(null);
-    setModalOpen(false);
+    setEditModalOpen(false);
   };
-// Función para cerrar el modal de agregar
+
   const closeAddModal = () => {
     setAddModalOpen(false);
   };
-  // Función para guardar los cambios en el modal
+
   const saveChanges = (editedProduct) => {
-  // Realizar una solicitud PUT al servidor para actualizar el producto
-  fetch(`http://localhost:3000/productos/${editedProduct.id}`, {
-    method: 'PUT',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(editedProduct),
+    // Realizar una solicitud PUT al servidor para actualizar el producto
+    fetch(`http://localhost:3000/productos/${editedProduct.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(editedProduct),
     })
-    .then((response) => response.json())
-    .then((data) => {
-      // Actualizar el estado de los productos con el producto editado
-      const updatedProducts = products.map((product) =>
-        product.id === editedProduct.id ? editedProduct : product
-      );
-      setProducts(updatedProducts);
-    })
-    .catch((error) => {
-      console.error('Error al guardar los cambios:', error);
-    });
+      .then((response) => response.json())
+      .then((data) => {
+        // Actualizar el estado de los productos con el producto editado
+        const updatedProducts = products.map((product) =>
+          product.id === editedProduct.id ? editedProduct : product
+        );
+        setProducts(updatedProducts);
+        setSearchResults(updatedProducts); // Actualizar los resultados de la búsqueda
+      })
+      .catch((error) => {
+        console.error('Error al guardar los cambios:', error);
+      });
   };
 
-
-  const deleteProduct = (product) => {
-  // Realizar una solicitud DELETE al servidor para eliminar el producto
-  fetch(`http://localhost:3000/productos/${product.id}`, {
-    method: 'DELETE',
-  })
-    .then((response) => {
-      if (response.status === 200) {
-        // Actualizar el estado de productos eliminando el producto
-        const updatedProducts = products.filter((p) => p.id !== product.id);
-        setProducts(updatedProducts);
-      } else {
-        console.error('Error al eliminar el producto:', response.statusText);
-      }
-    })
-    .catch((error) => {
-      console.error('Error al eliminar el producto:', error);
-    });
-};
-
-const addProduct = (newProduct) => {
-    // Realizar una solicitud POST al servidor para agregar el producto
+  const addProduct = (newProduct) => {
     fetch('http://localhost:3000/productos', {
       method: 'POST',
       headers: {
@@ -97,15 +75,22 @@ const addProduct = (newProduct) => {
     })
       .then((response) => response.json())
       .then((data) => {
-        // Agregar el nuevo producto a la lista de productos
         setProducts([...products, data]);
+        setSearchResults([...searchResults, data]); // Agregar el nuevo producto a los resultados de la búsqueda
       })
       .catch((error) => {
         console.error('Error al agregar un nuevo producto:', error);
       });
   };
 
-
+  const handleSearchChange = (e) => {
+    const { value } = e.target;
+    setSearchTerm(value); // Actualizar el término de búsqueda
+    const filteredResults = products.filter((product) =>
+      product.nombreProducto.toLowerCase().includes(value.toLowerCase())
+    );
+    setSearchResults(filteredResults); // Actualizar los resultados de la búsqueda
+  };
 
   return (
     <div className="container">
@@ -117,25 +102,30 @@ const addProduct = (newProduct) => {
         <div className="topBar">
           <div className="inputContainer">
             <label htmlFor="buscarInput">Buscar:</label>
-            <input type="text" id="buscarInput" />
+            <input
+              type="text"
+              id="buscarInput"
+              value={searchTerm}
+              onChange={handleSearchChange}
+            />
           </div>
-
           <button className="buttonStyle" onClick={openAddModal}>
             + Agregar producto
           </button>
         </div>
 
         <div className="mainContent">
-          <ProductTable products={products} onEdit={openEditModal} onDelete = {deleteProduct} />
+          <ProductTable products={searchResults} onEdit={openEditModal} />
         </div>
       </div>
 
       <EditProductModal
-        isOpen={isModalOpen}
+        isOpen={isEditModalOpen}
         onClose={closeEditModal}
         product={selectedProduct}
         onSave={saveChanges}
       />
+
       <AddProductModal
         isOpen={isAddModalOpen}
         onClose={closeAddModal}
